786cfd3ba4efb9b51179e940de1fd1b60216d742
feanorr <35889861+feanorr@users.noreply.github.com>
2018-01-28 15:04:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,18 +6,18 @@
 module.exports = {
   discord: {
     // The Discord token of the Bot to post through.
-    token: process.env.NDA3MTIzNTgyNDgxNzI3NDg4.DU8_1Q.kEYJ3PHyEsyV4vCLYPH3J6I9I7Y,
+    token: process.env.DISCORD_TOKEN=NDA3MTIzNTgyNDgxNzI3NDg4.DU8_1Q.kEYJ3PHyEsyV4vCLYPH3J6I9I7Y,
     // The ID of the discord channel to post battleboard infos to.
-    feedChannelId: process.env.407116046105837568,
+    feedChannelId: process.env.ALBION_FEED_CHANNEL_ID=407116046105837568,
     // The ID of the discord channel to post albion status infos to.
-    statusChannelId: process.env.407116046105837568
+    statusChannelId: process.env.ALBION_STATUS_CHANNEL_ID
   },
   guild: {
     // The name of your guild (or guilds, if the guild is large).
-    guilds: process.env.MOTHERLAND
+    guilds: process.env.ALBION_GUILDS="MOTHERLAND"
     ? process.env.ALBION_GUILDS.split(',') : [],
     // The alliance your guild belongs to
-    alliance: process.env.ROD
+    alliance: process.env.ALBION_ALLIANCE="ROD"
   },
   battle: {
     // Min players to report as battle
